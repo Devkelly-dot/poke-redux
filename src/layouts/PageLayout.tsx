a6948755872b71900061bcf2da4fc6eff6175451
2toesxtra <devkelly9622@gmail.com>
2022-12-21 15:20:15 -0500

    page layout change

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -9,11 +9,11 @@ const PageLayout : React.FC<Props> = ({children}) =>{
     return(
         <>
             <div>Nav</div>
-            <div className="grid sm:grid-cols-1 md:grid-cols-10 px-5 md:px-40">
-                <div className="md:col-span-8">
+            <div className="grid sm:grid-cols-1 md:grid-cols-10 px-5 md:px-24">
+                <div className="md:col-span-7 bg-slate-200">
                     {children}
                 </div>
-                <div className="order-first md:order-none">
+                <div className="order-first md:col-span-3 md:order-none bg-slate-300">
                     Sidebar
                 </div>
             </div>
