5306726f045f8927785b3baddf06fd6d15e3da9b
2toesxtra <devkelly9622@gmail.com>
2022-12-27 17:14:36 -0500

    fetch the types on the poke info page and updated PokeType to have moves, abilities, etc

--- src/pages/PokemonInfo.tsx
+++ src/pages/PokemonInfo.tsx
@@ -2,16 +2,63 @@ import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch, } from 'react-redux';
 import { RootState } from "../app/store";
 import pokemonTypeStyles  from '../partials/pokemon/pokemonTypeStyles'
+import { update_pokemon } from '../partials/pokemon/allPokemonSlice';
+import info_axios from '../lib/api/pokeinfo_api'
+import { useEffect, useState } from 'react';
+import { PokeType } from "../partials/pokemon/definePokemon";
 
 export default function PokemonInfo()
 {
+    const dispatch = useDispatch()
     const {pokemonName} = useParams();
     let displayName = ""
     if(pokemonName)
         displayName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
 
     const all_pokemon = useSelector((state:RootState)=>state.allPokemon.pokemon);
-    const myPokemon = all_pokemon.find((p)=>p.name===pokemonName)
+
+    const [myPokemon,setMyPokemon] = useState<PokeType>()
+
+    useEffect(()=>{
+        async function fetch_pokeInfo(pokemon:PokeType)
+        {
+            try{
+                const name = pokemon.name;
+                let request = await info_axios.get(`/pokemon/${name}`);
+                if(request.status!==200)
+                {
+                    throw new Error(`Request failed with status code ${request.status}`);
+                }
+                const fetch_types = request.data.types;
+                let types = []
+
+                for(let i in fetch_types)
+                {
+                    types.push(fetch_types[i].type.name)
+                }
+
+                const new_pokemon:PokeType = {
+                    name: pokemon.name,
+                    sprite:pokemon.sprite,
+                    type:types,
+                    hasFetched:true,
+                }
+                dispatch(update_pokemon({name: new_pokemon.name,pokemon: new_pokemon}))
+                setMyPokemon(new_pokemon);
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        const target_pokemon = all_pokemon.find((p)=>p.name===pokemonName);
+        setMyPokemon(target_pokemon)
+        if(!target_pokemon?.hasFetched)
+        {
+            if(target_pokemon)
+                fetch_pokeInfo(target_pokemon)
+        }
+    },[all_pokemon])
 
     return(
         <>
@@ -27,7 +74,7 @@ export default function PokemonInfo()
                         <div className='flex justify-center'>
                             {
                                 myPokemon.type?.map((type)=>
-                                    <div className={`${pokemonTypeStyles[type]} font-semibold p-1 rounded text-sm my-1`}>{type}</div>
+                                    <div className={`${pokemonTypeStyles[type]} font-semibold p-1 rounded text-sm my-1`} key={`class:${type}`}>{type}</div>
                                 )
                             }
                         </div>
--- src/partials/pokemon/definePokemon.tsx
+++ src/partials/pokemon/definePokemon.tsx
@@ -1,5 +1,23 @@
+type AbilityType = {
+    name: string,
+    description?: string,
+    url?:string,
+}
+
+type StatType = {
+    base_state: number,
+    effort: number,
+    name: string,
+}
+
 export type PokeType = {
     name: string,
     sprite?: string,
     type?: string[],
+    ability?:AbilityType[],
+    height?:string,
+    weight?:string,
+    move?:string[],
+    stat?:StatType[],
+    hasFetched?:boolean,
 }
