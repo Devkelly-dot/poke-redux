683fbbd0f3eecf76df0b02da2304e24da6a0237f
2toesxtra <devkelly9622@gmail.com>
2022-12-26 23:47:27 -0500

    refactored - brought initial pokemon fetch to the app component instead of the random pokebox component

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { createRoot } from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
+import App from './App';
+import { store } from './app/store';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-            <Route index element={<Home/>}/>
-        </Routes> 
-      </BrowserRouter> 
-    </Provider>
+  <Provider store={store}>
+      <App/>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/partials/pokemon/RandomPokeContainer.tsx
+++ src/partials/pokemon/RandomPokeContainer.tsx
@@ -19,84 +19,14 @@ export default function RandomPokeContainer()
     const all_pokemon = useSelector((state:RootState)=>state.allPokemon.pokemon);
 
     useEffect(()=>{ // initial setup
-        async function setAllPokemon()
+        function initial_setup()
         {
-            let request = await info_axios.get('/generation/generation-i')
-            const local_all_pokemon:PokeType[] = [];
-            for(let i = 0; i<request.data.pokemon_species.length; i++)
-            {
-                const pokemon = request.data.pokemon_species[i];
-                const name:string = pokemon.name;
-                let image_url_name = name;
-
-                if(name === 'nidoran-f') // the image url's name is slightly different for some specific cases
-                    image_url_name = 'nidoran_f'
-                if(name === 'nidoran-m')
-                    image_url_name = 'nidoran_m'
-                if(name === 'mr-mime')
-                    image_url_name = 'mr.mime'
-
-                const url = `https://projectpokemon.org/images/normal-sprite/${image_url_name}.gif`
-
-                const dispatch_pokemon: PokeType = {
-                    name: name,
-                    sprite: url,
-                }
-
-                dispatch(add_to_all(dispatch_pokemon))
-                local_all_pokemon.push(dispatch_pokemon)
-            }
-
-            request = await info_axios.get('/generation/generation-ii')
-            for(let i = 0; i<request.data.pokemon_species.length; i++)
-            {
-                const pokemon = request.data.pokemon_species[i];
-                let name:string = pokemon.name;
-                const url = `https://projectpokemon.org/images/normal-sprite/${name}.gif`
-
-                const dispatch_pokemon: PokeType = {
-                    name: name,
-                    sprite: url,
-                }
-
-                dispatch(add_to_all(dispatch_pokemon))
-                local_all_pokemon.push(dispatch_pokemon)
-            }
-
-            request = await info_axios.get('/generation/generation-iii')
-            for(let i = 0; i<request.data.pokemon_species.length; i++)
-            {
-                const pokemon = request.data.pokemon_species[i];
-                let name:string = pokemon.name;
-                const url = `https://projectpokemon.org/images/normal-sprite/${name}.gif`
-
-                const dispatch_pokemon: PokeType = {
-                    name: name,
-                    sprite: url,
-                }
-
-                dispatch(add_to_all(dispatch_pokemon))
-                local_all_pokemon.push(dispatch_pokemon)
-            }
-
-            return local_all_pokemon;
-        }
-
-        async function initial_setup()
-        {
-            if(all_pokemon.length < 1)
-            {
-                let local_all_pokemon = await setAllPokemon();
-                setDisplayFromArray(local_all_pokemon.slice(page*poke_count,(page*poke_count)+poke_count))
-            }
-            else
-            {
-                setDisplayFromArray(all_pokemon.slice(page*poke_count,(page*poke_count)+poke_count));
-            }
+            console.log("initial setup")
+            setDisplayFromArray(all_pokemon.slice(page*poke_count,(page*poke_count)+poke_count));
         }
 
         initial_setup();
-    },[])
+    },[all_pokemon.length])
 
     function handlePageSwitch(new_page:number)
     {
